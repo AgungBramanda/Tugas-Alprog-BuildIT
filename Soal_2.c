e2c4835a5846e516b4ebb925eb1a723e4acbe32f
bramandaagung <bramandaagung31@gmail.com>
2024-09-12 09:12:05 +0800

    third commit

---
+++ Soal_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char string1[101], string2[101];
+
+    scanf("%s", string1);
+    scanf("%s", string2);
+
+    if (strlen(string1) != strlen(string2))
+    {
+        // Jika panjangnya berbeda
+        printf("BERBEDA\n");
+    }
+    else
+    {
+
+        if (strcmp(string1, string2) == 0)
+        {
+
+            printf("IDENTIK\n");
+        }
+        else
+        {
+
+            printf("MIRIP\n");
+        }
+    }
+
+    return 0;
+}
